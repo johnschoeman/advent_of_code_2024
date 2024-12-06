6d007c160f6a4bfa6280fdd408d510c4f25ef877
John Schoeman <johnschoeman1617@gmail.com>
2024-12-06 18:56:57 -0500

    Refactor with crates

--- day-02/src/part_1.rs
+++ day-02/src/part_1.rs
@@ -1,56 +1,91 @@
+use itertools::Itertools;
+use miette::miette;
+use nom::{
+    character::complete::{self, newline, space1},
+    multi::separated_list1,
+    IResult,
+};
 use std::error::Error;
 use std::fs;
+use tracing::{info, instrument};
 
 const FILE_PATH: &str = "./input1.txt";
 
-pub fn run() -> Result<i32, Box<dyn Error>> {
+#[tracing::instrument]
+pub fn run() -> Result<String, Box<dyn Error>> {
     let contents = fs::read_to_string(FILE_PATH)?;
-
-    let sum = count_safe_reports(&contents);
-
-    Ok(sum)
+    match process(&contents) {
+        Ok(result) => Ok(result),
+        Err(e) => Err(e.into()),
+    }
 }
 
-fn count_safe_reports(contents: &str) -> i32 {
-    let result = contents.lines().filter(|line| is_safe_report(line)).count() as i32;
-    result
+enum Direction {
+    Increasing,
+    Decreasing,
 }
 
-fn is_safe_report(line: &str) -> bool {
-    let words: Vec<&str> = line.split_whitespace().collect();
-    let numbers = words
+fn process(input: &str) -> miette::Result<String> {
+    let (_, reports) = parse(input).map_err(|e| miette!("parse failed: {}", e))?;
+    let result = reports
         .iter()
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
-
-    let all_desc_or_asc = is_desc_or_asc(&numbers);
-    let all_within_bounds = is_all_within_bounds(&numbers);
-
-    return all_desc_or_asc && all_within_bounds;
+        .map(|report| check_safety(report))
+        .filter(|safety| safety.is_ok())
+        .count();
+    Ok(result.to_string())
 }
 
-fn is_desc_or_asc(numbers: &Vec<i32>) -> bool {
-    let mut is_desc = true;
-    let mut is_asc = true;
-    for i in 0..numbers.len() - 1 {
-        if numbers[i] < numbers[i + 1] {
-            is_desc = false;
-        }
-        if numbers[i] > numbers[i + 1] {
-            is_asc = false;
-        }
+#[instrument]
+fn check_safety(report: &Report) -> Result<(), String> {
+    let mut direction: Option<Direction> = None;
+    for (a, b) in report.iter().tuple_windows() {
+        let diff = a - b;
+        match diff.signum() {
+            -1 => match direction {
+                Some(Direction::Increasing) => {
+                    return Err(format!("{} {} switched to increasing", a, b));
+                }
+                Some(Direction::Decreasing) => {
+                    if !(1..=3).contains(&diff.abs()) {
+                        return Err(format!("{} {} diff value is {}", a, b, diff.abs()));
+                    } else {
+                        continue;
+                    }
+                }
+                None => {
+                    direction = Some(Direction::Decreasing);
+                }
+            },
+            1 => match direction {
+                Some(Direction::Decreasing) => {
+                    return Err(format!("{} {} switched to decreasing", a, b));
+                }
+                Some(Direction::Increasing) => {
+                    if !(1..=3).contains(&diff.abs()) {
+                        return Err(format!("{} {} diff value is {}", a, b, diff.abs()));
+                    } else {
+                        continue;
+                    }
+                }
+                None => {
+                    direction = Some(Direction::Increasing);
+                }
+            },
+            0 => {
+                Err(format!("{} {} diff value is 0", a, b))?;
+            }
+            _ => {
+                panic!("unexpected signum: {}", diff.signum());
+            }
+        };
     }
-    is_desc || is_asc
+    Ok(())
 }
 
-fn is_all_within_bounds(numbers: &Vec<i32>) -> bool {
-    for i in 0..numbers.len() - 1 {
-        let diff = (numbers[i] - numbers[i + 1]).abs();
-        if diff < 1 || diff > 3 {
-            return false;
-        }
-    }
-    true
+type Report = Vec<i32>;
+
+fn parse(input: &str) -> IResult<&str, Vec<Report>> {
+    separated_list1(newline, separated_list1(space1, complete::i32))(input)
 }
 
 #[cfg(test)]
@@ -58,7 +93,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn count_safe_reports_test() {
+    fn test_process() -> miette::Result<()> {
         let contents = "\
 7 6 4 2 1
 1 2 7 8 9
@@ -67,6 +102,7 @@ mod tests {
 8 6 4 4 1
 1 3 6 7 9
 ";
-        assert_eq!(2, count_safe_reports(contents));
+        assert_eq!("2", process(contents)?);
+        Ok(())
     }
 }
