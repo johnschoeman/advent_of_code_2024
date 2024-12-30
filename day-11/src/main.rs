e6ce3961c94bcbde1f20a16cd2fb4abc6b907508
John Schoeman <johnschoeman1617@gmail.com>
2024-12-30 15:48:49 -0500

    Day 11 - Part 1

---
+++ day-11/src/main.rs
@@ -0,0 +1,15 @@
+mod part1;
+// mod part2;
+
+fn main() {
+    let result1 = part1::run();
+    match result1 {
+        Ok(sum) => println!("Part 1 Result: {}", sum),
+        Err(e) => println!("Error: {}", e),
+    }
+    // let result2 = part2::run();
+    // match result2 {
+    //     Ok(sum) => println!("Part 2 Result: {}", sum),
+    //     Err(e) => println!("Error: {}", e),
+    // }
+}
---
+++ day-11/src/part1.rs
@@ -0,0 +1,77 @@
+use std::error::Error;
+use std::fs;
+
+const FILE_PATH: &str = "./input.txt";
+
+pub fn run() -> Result<String, Box<dyn Error>> {
+    let contents = fs::read_to_string(FILE_PATH)?;
+    match process(&contents) {
+        Ok(result) => Ok(result.to_string()),
+        Err(e) => Err(e.into()),
+    }
+}
+
+// type Stone<'a> = &'a str;
+// type Stones<'a> = Vec<Stone<'a>>;
+
+type Stone = String;
+type Stones = Vec<Stone>;
+
+fn process(input: &str) -> Result<u32, String> {
+    let mut stones = input.split_whitespace().map(|s| s.to_string()).collect::<Stones>();
+
+    for x in 0..25 {
+        // print_stones(&stones);
+        println!("{} {}", x, stones.len());
+        stones = blink(stones);
+    }
+
+    Ok(stones.len() as u32)
+}
+
+fn blink(stones: Stones) -> Stones {
+    stones.iter().flat_map(|stone| process_stone(stone.clone())).collect()
+}
+
+fn process_stone(stone: Stone) -> Stones {
+    if stone == "0" {
+        return vec!["1".to_string()];
+    }
+
+    if stone.len() % 2 == 0 {
+        let left = &stone[..stone.len() / 2];
+        let right = remove_leading_zeros(&stone[stone.len() / 2..]);
+
+        return vec![left.to_string(), right.to_string()];
+    }
+
+    let next = (stone.parse::<u64>().unwrap() * 2024).to_string();
+
+    vec![next]
+}
+
+fn remove_leading_zeros(input: &str) -> String {
+    let result = input.trim_start_matches('0').to_string();
+    if result.is_empty() {
+        return "0".to_string();
+    } else {
+        return result;
+    }
+}
+
+fn print_stones(stones: &Stones) {
+    let stones = stones.iter().map(|stone| stone.to_string()).collect::<Vec<String>>();
+    println!("{}", stones.join(" "));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> Result<(), String> {
+        let contents = "125 17";
+        assert_eq!(55312, process(contents)?);
+        Ok(())
+    }
+}
